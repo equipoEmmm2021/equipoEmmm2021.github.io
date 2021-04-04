661f5d87d58b23cfcbafd6fa5bd7d5b965f961e2
equipoEmmm2021 <81917469+equipoEmmm2021@users.noreply.github.com>
2021-04-05 01:23:21 +0200

    test

---
+++ js/local_js.js
@@ -0,0 +1,64 @@
+//Functions to show different content blocks in mobile
+
+function showCampaignM(){
+    document.getElementById("campaignM").classList.remove("hide");
+    document.getElementById("buttonCampaignM").classList.add("menuActive");
+    document.getElementById("faqsM").classList.add("hide");
+    document.getElementById("buttonFaqsM").classList.remove("menuActive");
+    document.getElementById("commentsM").classList.add("hide");
+    document.getElementById("buttonCommentsM").classList.remove("menuActive");
+
+}
+
+function showFaqsM(){
+    document.getElementById("campaignM").classList.add("hide");
+    document.getElementById("buttonCampaignM").classList.remove("menuActive");
+    document.getElementById("faqsM").classList.remove("hide");
+    document.getElementById("buttonFaqsM").classList.add("menuActive");
+    document.getElementById("commentsM").classList.add("hide");
+    document.getElementById("buttonCommentsM").classList.remove("menuActive");
+
+}
+
+function showCommentsM(){
+    document.getElementById("campaignM").classList.add("hide");
+    document.getElementById("buttonCampaignM").classList.remove("menuActive");
+    document.getElementById("faqsM").classList.add("hide");
+    document.getElementById("buttonFaqsM").classList.remove("menuActive");
+    document.getElementById("commentsM").classList.remove("hide");
+    document.getElementById("buttonCommentsM").classList.add("menuActive");
+
+}
+
+//Functions to show different content blocks in desktop
+
+function showCampaignD(){
+    console.log("Funciona");
+    document.getElementById("campaignD").classList.remove("hide");
+    document.getElementById("buttonCampaignD").classList.add("menuActive");
+    document.getElementById("faqsD").classList.add("hide");
+    document.getElementById("buttonFaqsD").classList.remove("menuActive");
+    document.getElementById("commentsD").classList.add("hide");
+    document.getElementById("buttonCommentsD").classList.remove("menuActive");
+    console.log("Funciona");
+}
+
+function showFaqsD(){
+    document.getElementById("campaignD").classList.add("hide");
+    document.getElementById("buttonCampaignD").classList.remove("menuActive");
+    document.getElementById("faqsD").classList.remove("hide");
+    document.getElementById("buttonFaqsD").classList.add("menuActive");
+    document.getElementById("commentsD").classList.add("hide");
+    document.getElementById("buttonCommentsD").classList.remove("menuActive");
+
+}
+
+function showCommentsD(){
+    document.getElementById("campaignD").classList.add("hide");
+    document.getElementById("buttonCampaignD").classList.remove("menuActive");
+    document.getElementById("faqsD").classList.add("hide");
+    document.getElementById("buttonFaqsD").classList.remove("menuActive");
+    document.getElementById("commentsD").classList.remove("hide");
+    document.getElementById("buttonCommentsD").classList.add("menuActive");
+
+}
